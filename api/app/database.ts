eb347a05021e0236c0783ad4de06a63c27bb5354
Kyle Stevens <kstevens715@gmail.com>
2017-05-30 21:54:14 -0400

    DRY up database config
    This DRY's up the database configuration and has both migrations and
    database functions use the DATABASE_URL environment variable. This will
    be useful when I add tests next so that I can easily configure a
    different database connection for the test environment.

--- api/app/database.ts
+++ api/app/database.ts
@@ -1,22 +1,20 @@
 import * as pg from 'pg';
+import * as url from 'url';
 
-// create a config to configure both pooling behavior
-// and client options
-// note: all config is optional and the environment variables
-// will be read if the config is not present
-var config = {
-  user: 'uname', //env var: PGUSER
-  database: 'rubyles', //env var: PGDATABASE
-  password: 'pw', //env var: PGPASSWORD
-  host: 'db', // Server hosting the postgres database
-  port: 5432, //env var: PGPORT
-  max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(':');
+
+const config = {
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: parseInt(params.port),
+  database: params.pathname.split('/')[1],
+  ssl: false,
+  max: 10,
+  idleTimeoutMillis: 30000,
 };
 
-//this initializes a connection pool
-//it will keep idle connections open for 30 seconds
-//and set a limit of maximum 10 idle clients
 const pool = new pg.Pool(config);
 
 pool.on('error', function (err: any, client: any) {
