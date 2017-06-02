44a59de157779d7fdc29a0862fa93a8de526f936
Kyle Stevens <kstevens715@gmail.com>
2017-06-01 23:11:12 -0400

    Insert remaining transactions columns

--- api/app/models/transactions.ts
+++ api/app/models/transactions.ts
@@ -2,7 +2,8 @@ import { query } from '../database';
 
 export class Transactions {
   static create(obj: any) {
-    query(`INSERT INTO transactions(payee) VALUES($1)`, [obj.payee], function(err: any, res: any) {
+    var values = [obj.payee, obj.referenceNumber, obj.date, obj.cleared, obj.paymentAmount, obj.depositAmount];
+    query(`INSERT INTO transactions("payee", "referenceNumber", "date", "cleared", "paymentAmount", "depositAmount") VALUES($1, $2, $3, $4, $5, $6)`, values, function(err: any, res: any) {
     });
   }
 
--- api/app/models/transactionsSpec.ts
+++ api/app/models/transactionsSpec.ts
@@ -1,11 +1,12 @@
 import { Transactions } from './transactions';
-import { expect } from 'chai';
+import { assert } from 'chai';
 import { query } from '../database';
 import 'mocha';
 //TODO: Replace all references to 'any'
 //TODO: Decide if callback is way to go, or if should use promise or RXjs
 //TODO: Log somewhere else?
 //TODO: You sure you don't want to use an ORM?
+//TODO: If not, should I at least abstract the SQL more?
 describe('Transactions', function() {
   beforeEach(function(done) {
     query(`DELETE FROM transactions`, [], function() { done()});
@@ -14,18 +15,31 @@ describe('Transactions', function() {
   describe('.find', function() {
     it('returns an empty array when no transactions exist', function(done: any) {
       Transactions.find(function(err: any, transactions: any) {
-        expect(transactions.length).to.equal(0);
+        assert.equal(transactions.length, 0);
         done();
       });
     });
 
     it('returns transactions that do exist', function(done: any) {
       Transactions.create({
-        payee: "Butcher Block"
+        payee: 'Butcher Block',
+        referenceNumber: '1234',
+        date: '2017-06-01',
+        cleared: true,
+        paymentAmount: 1001,
+        depositAmount: 1002,
       });
       Transactions.find(function(err: any, transactions: any) {
-        expect(transactions.length).to.equal(1);
-        expect(transactions[0].payee).to.equal('Butcher Block');
+        let transaction = transactions[0];
+
+        //TODO: Assert about id?
+        assert.equal(transactions.length, 1);
+        assert.equal(transaction.payee, 'Butcher Block');
+        assert.equal(transaction.referenceNumber, '1234');
+        assert.equal(transaction.date.toUTCString(), new Date('2017-06-01').toUTCString());
+        assert.equal(transaction.paymentAmount, 1001);
+        assert.equal(transaction.depositAmount, 1002);
+        assert.isTrue(transaction.cleared);
         done();
       });
     });
