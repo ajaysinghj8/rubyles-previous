b8e9c2ae37b72879f57f01fd5952ec95069c5beb
Kyle Stevens <kstevens715@gmail.com>
2017-06-01 22:18:52 -0400

    Implement function to create a transaction

--- api/app/models/transactions.ts
+++ api/app/models/transactions.ts
@@ -2,16 +2,15 @@ import { query } from '../database';
 
 export class Transactions {
   static create(obj: any) {
+    query(`INSERT INTO transactions(payee) VALUES($1)`, [obj.payee], function(err: any, res: any) {
+    });
   }
 
   static find(callback: any) {
-    var select = 'SELECT "id", "payee", "referenceNumber", "date", "cleared", "paymentAmount", "depositAmount" FROM "transactions"';
+    var select = `SELECT "id", "payee", "referenceNumber", "date", "cleared", "paymentAmount", "depositAmount" FROM "transactions"`;
+
     query(select, [], function(err: any, res: any) {
-      let transactions = [];
-      if (res) {
-        transactions = res.rows;
-      }
-      callback.call(this, err, transactions);
+      callback.call(this, err, res.rows);
     });
   }
 }
--- api/app/models/transactionsSpec.ts
+++ api/app/models/transactionsSpec.ts
@@ -1,18 +1,33 @@
 import { Transactions } from './transactions';
 import { expect } from 'chai';
+import { query } from '../database';
 import 'mocha';
 //TODO: Replace all references to 'any'
 //TODO: Decide if callback is way to go, or if should use promise or RXjs
+//TODO: Log somewhere else?
+//TODO: You sure you don't want to use an ORM?
 describe('Transactions', function() {
-  it('.find returns an empty array when no transactions exist', function(done: any) {
+  beforeEach(function(done) {
+    query(`DELETE FROM transactions`, [], function() { done()});
+  });
 
-    // Transactions.create({
-    //   payee: "Butcher Block"
-    // });
+  describe('.find', function() {
+    it('returns an empty array when no transactions exist', function(done: any) {
+      Transactions.find(function(err: any, transactions: any) {
+        expect(transactions.length).to.equal(0);
+        done();
+      });
+    });
 
-    Transactions.find(function(err: any, transactions: any) {
-      expect(transactions.length).to.equal(0);
-      done();
+    it('returns transactions that do exist', function(done: any) {
+      Transactions.create({
+        payee: "Butcher Block"
+      });
+      Transactions.find(function(err: any, transactions: any) {
+        expect(transactions.length).to.equal(1);
+        expect(transactions[0].payee).to.equal('Butcher Block');
+        done();
+      });
     });
   });
 });
